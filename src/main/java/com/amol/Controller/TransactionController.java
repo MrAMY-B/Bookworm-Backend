e6853e4afdf7b36f76b114df40ee3529174ca263
MrAMY-B <amolbharsakle1@gmail.com>
2021-09-21 18:06:28 +0530

    Trasactons Added

--- src/main/java/com/amol/Controller/TransactionController.java
+++ src/main/java/com/amol/Controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.amol.Controller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amol.Entity.Shelf;
 import com.amol.Entity.Transaction;
 import com.amol.Repository.ShelfRepository;
 import com.amol.Repository.TransactionRepository;
@@ -54,37 +56,81 @@ public class TransactionController {
 		return this.transRepo.findTransactionByProductId(id);
 	}
 	
-	@PostMapping("/")
-	public Transaction saveOneTransaction(@RequestBody Transaction tran) {
-		return this.transRepo.save(tran);
-	}
+//	@PostMapping("/")
+//	public Transaction saveOneTransaction(@RequestBody Transaction tran) {
+//		return this.transRepo.save(tran);
+//	}
 	
 	//==============MODIFIED - 18Sept-2021
-//	@PostMapping("/")
-//	public String saveTransaction(@RequestBody Transaction trans) {
-//		if(trans!=null) {
-//			trans.setDate(new Date());
-//			Transaction  t = this.transRepo.save(trans);
-//			
-//			if(t!=null && t.getTr_id()>0 ) {
-//				Shelf shelf = new Shelf();
-//				
-//				t.getProducts()
-//					.forEach( prod ->{
-//						shelf.getProducts().add(prod);
-//					});
-//				
-//				shelf.setUser(t.getUser());
-//				shelf.setTr_type(t.getTr_type());
-//				shelf.setProd_expiry(new java.sql.Date(System.currentTimeMillis()));
-//				
-//				Shelf ss = this.shelfRepo.save(shelf);
-//				System.out.println("\n\n ========================\n"+ss+" \n\n");
-//			}
-//			return "TRANSACTION SAVED SUCCESSFULLY";
-//		}
-//		return "FAILED..!! PLEASE TRY AGAIN";
-//	}
+	@PostMapping("/purchase/")
+	public String buyProductTransaction(@RequestBody Transaction trans) {
+		if(trans!=null) {
+			trans.setDate(new Date());
+			trans.setStatus("SUCCESSFULL");
+			Transaction  t = this.transRepo.save(trans);
+			
+			if(t!=null && t.getTr_id()>0 ) {
+				Shelf shelf = new Shelf();
+				
+				shelf.setProduct(t.getProduct());
+				
+				shelf.setUser(t.getUser());
+				shelf.setTr_type(t.getTr_type());
+				
+				//++++++++++Please Change By Default 1 year
+				shelf.setProd_expiry(new java.sql.Date(System.currentTimeMillis()+(3600000*24*365)));
+				
+				Shelf ss = this.shelfRepo.save(shelf);
+				if(ss!=null)
+					return "SUCCESSFULL";
+			}
+			return "SUCCESSFULL";
+		}
+		return "FAILED";
+	}
+	
+	@PostMapping("/rent/")
+	public String rentProductTransaction(@RequestBody Transaction trans) {
+		if(trans!=null) {
+			trans.setDate(new Date());
+			trans.setStatus("SUCCESSFULL");
+			Transaction  t = this.transRepo.save(trans);
+			
+			if(t!=null && t.getTr_id()>0 ) {
+				Shelf shelf = new Shelf();
+				
+				shelf.setProduct(t.getProduct());
+				
+				shelf.setUser(t.getUser());
+				shelf.setTr_type(t.getTr_type());
+				
+				//++++++++++Please Change By Default 10 days
+				shelf.setProd_expiry(new java.sql.Date(System.currentTimeMillis()+(3600000*24*30)));
+				
+				Shelf ss = this.shelfRepo.save(shelf);
+				if(ss!=null)
+					return "SUCCESSFULL";
+			}
+			return "SUCCESSFULL";
+		}
+		return "FAILED";
+	}
+	
+	@PostMapping("/lent/")
+	public String lentProductTransaction(@RequestBody Shelf shelf) {
+		
+		
+				shelf.setTr_type("LENTED");
+				
+				//++++++++++Please Change By Default 1 month
+				shelf.setProd_expiry(new java.sql.Date(System.currentTimeMillis()+(3600000*24*30)));
+				
+				Shelf ss = this.shelfRepo.save(shelf);
+				if(ss!=null)
+					return "SUCCESSFULL";
+				
+			return "FAILED";
+	}
 	
 	@PostMapping("/all")
 	public String saveAllTransaction(@RequestBody List<Transaction> trans) {
---
+++ src/main/java/com/amol/Testing.java
@@ -0,0 +1,13 @@
+package com.amol;
+
+import java.sql.Date;
+
+public class Testing {
+//	public static void main(String[] args) {
+//		
+//		System.out.println(new Date(System.currentTimeMillis()+(3600000*24*13)));
+//		
+//		
+//	}
+
+}
